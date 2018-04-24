db43ead216d75ec63004332fee1fb31fc416c2fb
ztcooper <ztcooper@outlook.com>
2018-04-24 21:20:16 +0800

    create HTMLDownloader

---
+++ HTMLDownloader.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+'''
+下载页面
+url = http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx?DocID=" + id
+'''
+import requests
+
+
+class HtmlDownloader(object):
+
+    def download(self, url):
+        headers = {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"}
+        r = requests.get(url, headers=headers)
+        if r.status_code = 200
+            return r.text
+        return None
--- URLManager.py
+++ URLManager.py
@@ -1,3 +1,7 @@
+# encoding:utf-8
+'''
+通过DocID构造接口链接
+'''
 import json
 from GetAPI import GetAPI
 
@@ -6,13 +10,7 @@ class UrlManager(object):
     def __init__(self):
         self.urls = set()
 
-    def get_DocID(self):
-        docids = []
-        Param = "全文检索:*"  # 搜索关键字
-        Index = 1  # 第几页
-        Page = 20  # 每页几条
-        Order = "法院层级"  # 排序标准
-        Direction = "asc"  # asc正序 desc倒序
+    def get_DocID(self, Param, Index, Page, Order, Direction):
         data = GetAPI().get_data(Param, Index, Page, Order, Direction)
         for i in range(1, Page + 1):
             docids.append(eval(json.loads(data))[i]["文书ID"])
