cb9f253ef63f0fabfa229a9292c2bd81b68d46a4
ztcooper <ztcooper@outlook.com>
2018-04-24 23:05:24 +0800

    create HTMLParser

---
+++ HTMLParser.py
@@ -0,0 +1,38 @@
+# encoding:utf-8
+'''
+解析页面，得到数据
+'''
+from bs4 import BeautifulSoup
+import re
+import json
+
+
+class HtmlParser(object):
+    def __init__(self):
+        self.item = dict()
+
+    def parse(self, source):
+        p = re.compile(r'jsonHtmlData =(.*?)}";')
+        target = eval(json.loads(p.findall(source)[0] + '}"'))
+
+        self.item['title'] = target['Title']		# 标题
+        self.item['pubdate'] = target['PubDate']		# 发布时间
+        html = target['Html']
+        # 提取正文
+        soup = BeautifulSoup(html, 'lxml')
+        divs = soup.find_all('div')
+        article = ""
+        for div in divs:
+            try:
+                article += div.get_text()
+            except TypeError:
+                continue
+        self.item['article'] = article.strip()  # 正文
+
+
+if __name__ == '__main__':
+    with open('test.txt') as f:
+        source = f.read()
+    s = HtmlParser()
+    s.parse(source)
+    print(s.item)
